f153311beb172ea195b8e21738e8ff254079df90
vivekpawar <vivekpawar932564@gmail.com>
2025-08-11 23:31:43 +0530

    Updated Flask YouTube downloader

--- app.py
+++ app.py
@@ -1,34 +1,33 @@
-from flask import Flask, request, send_file, render_template_string
+from flask import Flask, request, send_file, render_template
 import yt_dlp
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    # Serve your HTML file
-    with open('index.html', 'r', encoding='utf-8') as f:
-        return f.read()
+    return render_template("index.html")
 
-@app.route('/download', methods=['POST'])
-def download():
-    url = request.form.get('url')
+@app.route("/download", methods=["POST"])
+def download_video():
+    url = request.form.get("url")
     if not url:
         return "No URL provided", 400
 
-    # Download options
-    ydl_opts = {
-        'format': 'best',
-        'outtmpl': 'downloaded_video.%(ext)s',
-        'quiet': True,
-    }
+    try:
+        # Download video to a temporary file
+        ydl_opts = {
+            'format': 'best',
+            'outtmpl': 'video.%(ext)s',
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info)
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=True)
-        filename = ydl.prepare_filename(info)
+        return send_file(filename, as_attachment=True)
 
-    # Send the file to the user
-    return send_file(filename, as_attachment=True)
+    except Exception as e:
+        return f"Error: {str(e)}", 500
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
